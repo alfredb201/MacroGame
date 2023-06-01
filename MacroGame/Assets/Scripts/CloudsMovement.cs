cb25bb941aafd4802d7a8e5144059a65a2228d7c
alfredb201 <alfiobuonaguro201@gmail.com>
2023-06-01 11:11:46 +0200

    fixed clouds movement
    fixed clouds movement

--- MacroGame/Assets/Scripts/CloudsMovement.cs
+++ MacroGame/Assets/Scripts/CloudsMovement.cs
@@ -6,11 +6,17 @@ public class CloudsMovement : MonoBehaviour
 {
     [SerializeField]
     private float _speed = 4.5f;
-
+    public PlayerTouchMovement touch;
+    private void Start()
+    {
+        touch = GameObject.Find("FlyingSprite").GetComponent<PlayerTouchMovement>();
+    }
     // Update is called once per frame
     void Update()
     {
         float _horizontalMovemet = Input.GetAxis("Horizontal");
         transform.Translate(Vector3.left * _horizontalMovemet * _speed * Time.deltaTime);
+        float _horizontalMovemetTouch = touch.MovementAmount.x;
+        transform.Translate(Vector3.left * _horizontalMovemetTouch * _speed * Time.deltaTime);
     }
 }
